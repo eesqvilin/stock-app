5ac75b695b84381598bd095e9623b2da0b106c00
esqvilin <emmanuel.esquilinvargas@gmail.com>
2026-04-29 23:30:01 -0400

    Add main method and initialize concurrent stock queue
    - Remove unused `java.io.IOException` import and add concurrency and time imports.
    - Introduce a static, thread-safe `stocks` queue using `ConcurrentLinkedQueue`.
    - Add a `main` method that initializes a `ScheduledExecutorService` with a single thread.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,22 @@
 package org.example;
 
-import java.io.IOException;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
+import java.time.Instant;
+import java.util.Queue;
+import java.util.concurrent.*;
+
+
 public class App {
+
+    private static final Queue<Stock> stocks = new ConcurrentLinkedQueue<>();
+
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    }
 
 
 
 }
+
